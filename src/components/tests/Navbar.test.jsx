7f8b53a5fcaffddc1420e1c4998babdaa0c35e23
Arch <wbhaoui@gmail.com>
2024-03-10 09:49:16 +0000

    Update Navbar tests

--- src/components/tests/Navbar.test.jsx
+++ src/components/tests/Navbar.test.jsx
@@ -1,23 +1,28 @@
-import {render, screen, getByRole, getByTestId, queryByTestId, getAllByRole} from "@testing-library/react"
-import { vi } from "vitest";
-import Navbar from "../Navbar/Navbar"
-import { Link } from "react-router-dom";
+import {
+  render,
+  screen,
+  getByRole,
+  getByTestId,
+  queryByTestId,
+  getAllByRole,
+} from "@testing-library/react";
+import { expect, vi } from "vitest";
+import Navbar from "../Navbar/Navbar";
 
 describe("Navbar component", () => {
   it("renders a nav element with links from prop array inside a <ul> element", () => {
-    render(<Navbar links={['a', 'b', 'c']}/>);
-    vi.mock('react-router-dom', () => {
-      const mod = vi.importActual('react-router-dom');
+    render(<Navbar links={["a", "b", "c"]} />);
+    vi.mock("react-router-dom", () => {
+      const mod = vi.importActual("react-router-dom");
       return {
         ...mod,
-        Link: ({to}) => <a href="">{to}</a>
-      }
-    })
+        Link: ({ to }) => <a href="">{to}</a>,
+      };
+    });
 
     const nav = screen.getByRole("navigation");
     const linkContainer = getByRole(nav, "list");
     const links = getAllByRole(linkContainer, "link");
-    console.log(links);
 
     expect(nav).toBeInTheDocument();
     expect(links.length).toEqual(3);
@@ -25,24 +30,42 @@ describe("Navbar component", () => {
     expect(links[1]).toHaveTextContent(/^b$/);
     expect(links[2]).toHaveTextContent(/^c$/);
   });
-  it("renders CartIcon component if isShopping prop is true", () => {
-    render(<Navbar isShopping={true} links={[]}/>);
-    vi.mock('../CartIcon/CartIcon', ()=> {
+  it("renders CartIcon component if activeLink is shop", () => {
+    render(<Navbar activeLink={"shop"} links={[]} />);
+    vi.mock("../CartIcon/CartIcon", () => {
       return {
-        default: () => <svg data-testid="icon"></svg>
-      }
-    })
+        default: () => <svg data-testid="icon"></svg>,
+      };
+    });
     const nav = screen.getByRole("navigation");
-    expect(getByTestId(nav, 'icon')).toBeInTheDocument();
-  })
+    expect(getByTestId(nav, "icon")).toBeInTheDocument();
+  });
   it("does not render CartIcon component if isShopping is false", () => {
-    render(<Navbar isShopping={false} links={[]}/>);
-    vi.mock('../CartIcon/CartIcon', ()=> {
+    render(<Navbar activeLink={"home"} links={[]} />);
+    vi.mock("../CartIcon/CartIcon", () => {
       return {
-        default: () => <svg data-testid="icon"></svg>
-      }
-    })
+        default: () => <svg data-testid="icon"></svg>,
+      };
+    });
     const nav = screen.getByRole("navigation");
-    expect(queryByTestId(nav, 'icon')).toBeNull();
-  })
-})
+    expect(queryByTestId(nav, "icon")).toBeNull();
+  });
+  it("renders active link with active-link class", () => {
+    render(<Navbar activeLink={"a"} links={["a", "b", "c"]} />);
+
+    vi.mock("react-router-dom", () => {
+      const mod = vi.importActual("react-router-dom");
+      return {
+        ...mod,
+        Link: ({ to, id }) => (
+          <a id={id} href="">
+            {to}
+          </a>
+        ),
+      };
+    });
+    const nav = screen.getByRole("navigation");
+    const links = getAllByRole(nav, "link");
+    expect(links[0].id).toMatch(/active/);
+  });
+});
