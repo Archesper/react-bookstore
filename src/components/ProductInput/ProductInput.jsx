4e883e0d1413b42e165cccd194c535188610350c
Arch <wbhaoui@gmail.com>
2024-03-11 12:03:50 +0000

    Add ProductInput styles

--- src/components/ProductInput/ProductInput.jsx
+++ src/components/ProductInput/ProductInput.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import styles from "./ProductInput.module.css"
 
 const ProductInput = ({ updateCart, productData }) => {
   const [inCart, setInCart] = useState(false);
@@ -6,16 +7,17 @@ const ProductInput = ({ updateCart, productData }) => {
   const updateInputValue = (value) => {setInputValue(value); updateCart(productData, parseInt(value))}
   if (inCart) {
     return (
-      <div className="product-input" data-testid="product-input">
-        <button onClick={()=> updateInputValue(inputValue  - 1)}>-</button>
+      <div className={styles["product-input"]} data-testid="product-input">
+        <button className={styles["input-btn"]} onClick={()=> updateInputValue(inputValue  - 1)}>-</button>
         <input onChange={(e)=> {updateInputValue(e.target.value)}} type="number" value={inputValue} name="cart-count" id="cart-count"/>
-        <button onClick={()=> updateInputValue(inputValue  + 1)}>+</button>
+        <button className={styles["input-btn"]} onClick={()=> updateInputValue(inputValue  + 1)}>+</button>
       </div>
     );
   } else {
     return (
       <div className="product-input" data-testid="product-input">
         <button
+          className={styles["add-cart-btn"]}
           onClick={() => {
             setInCart(true);
             updateInputValue(1);
