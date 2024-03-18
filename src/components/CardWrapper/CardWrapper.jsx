b2b4ac908e40e18994beeef4cdeff129d9e4d80d
Arch <wbhaoui@gmail.com>
2024-03-18 15:34:32 +0000

    Add CardWrapper styles

--- src/components/CardWrapper/CardWrapper.jsx
+++ src/components/CardWrapper/CardWrapper.jsx
@@ -1,11 +1,12 @@
-import { useOutletContext } from "react-router-dom";
+import  styles from "./CardWrapper.module.css"
 import Card from "../Card/Card";
+
 
 const CardWrapper = ({productData, updateCart}) => {
   const cards = productData.map((data) => <Card key={data.id} updateCart={updateCart} productData={data}/>);
   return (
     <main>
-      <div className="cards-wrapper">
+      <div className={styles["card-wrapper"]}>
         {cards}
       </div>
     </main>
