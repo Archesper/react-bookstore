732ffc0b93bdcfaa77f7ca2c0aa41262c3b8ed7b
Arch <wbhaoui@gmail.com>
2024-03-18 14:58:09 +0000

    Add input sanitization to ProductInput

--- src/components/ProductInput/ProductInput.jsx
+++ src/components/ProductInput/ProductInput.jsx
@@ -1,16 +1,43 @@
 import { useState } from "react";
-import styles from "./ProductInput.module.css"
+import styles from "./ProductInput.module.css";
 
 const ProductInput = ({ updateCart, productData }) => {
   const [inCart, setInCart] = useState(false);
   const [inputValue, setInputValue] = useState(0);
-  const updateInputValue = (value) => {setInputValue(value); updateCart(productData, parseInt(value))}
+  const updateInputValue = (value) => {
+    if (parseInt(value) === 0) {
+      setInCart(false);
+    }
+    const sanitizedValue = value.toString().replaceAll(/\D/g, '');
+    setInputValue(sanitizedValue);
+    updateCart(productData, parseInt(sanitizedValue));
+  };
   if (inCart) {
     return (
       <div className={styles["product-input"]} data-testid="product-input">
-        <button className={styles["input-btn"]} onClick={()=> updateInputValue(parseInt(inputValue)  - 1)}>-</button>
-        <input onChange={(e)=> {updateInputValue(e.target.value)}} type="number" value={inputValue} name="cart-count" id="cart-count"/>
-        <button className={styles["input-btn"]} onClick={()=> updateInputValue(parseInt(inputValue)  + 1)}>+</button>
+        <button
+          className={styles["input-btn"]}
+          onClick={() => updateInputValue(parseInt(inputValue) - 1)}
+        >
+          -
+        </button>
+        <input
+          onChange={(e) => {
+            updateInputValue(e.target.value);
+          }}
+          type="number"
+          min="0"
+          step="1"
+          value={inputValue}
+          name="cart-count"
+          id="cart-count"
+        />
+        <button
+          className={styles["input-btn"]}
+          onClick={() => updateInputValue(parseInt(inputValue) + 1)}
+        >
+          +
+        </button>
       </div>
     );
   } else {
