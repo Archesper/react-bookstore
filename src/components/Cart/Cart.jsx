22a7b4e495b87794b5aedb10f43fc7daa7de0eff
Arch <wbhaoui@gmail.com>
2024-03-26 14:03:58 +0000

    Add Cart side menu sliding animation

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,17 +1,19 @@
 import { useOutletContext } from "react-router-dom";
-
+import styles from "./Cart.module.css"
+import modalStyles from "../Modal/Modal.module.css"
+import cover from "../../../public/cover.webp"
 const Cart = () => {
   const { cartData } = useOutletContext();
   const totalPrice = cartData.reduce((previous, current) => previous + current.price * current.quantity, 0);
   const cartItems = cartData.map((cartItem) => (
     <div key={cartItem.id} className="cart-item" data-testid="cart-item">
-      <img src={cartItem.image} alt={cartItem.description} />
+      <img src={cover} alt={cartItem.description} />
       <h3>{cartItem.title}</h3>
       <p>{cartItem.price}</p>
     </div>
   ));
   return (
-    <div className="cart">
+    <div className={styles.cart + " " + modalStyles.cart}>
       {cartItems}
       <div><span>Price:</span> <span data-testid="price">{totalPrice}</span></div>
     </div>
