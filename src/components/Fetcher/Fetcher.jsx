9119fcc34e3758a9203defc57b847e9084f9bb4b
Arch <wbhaoui@gmail.com>
2024-04-15 09:20:56 +0100

    Make amount of books per author random and shuffle overall books

--- src/components/Fetcher/Fetcher.jsx
+++ src/components/Fetcher/Fetcher.jsx
@@ -2,44 +2,59 @@ import { useEffect, useState } from "react";
 import { useOutletContext } from "react-router-dom";
 import CardWrapper from "../CardWrapper/CardWrapper";
 
-
 const Fetcher = () => {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
   const [data, setData] = useState(null);
-  const {updateCart} = useOutletContext();
+  const { updateCart } = useOutletContext();
   const f = async () => {
     const data = await fetchData();
     setData(data);
     setLoading(false);
-  }
+  };
   useEffect(() => {
     const fetchData = async () => {
-      const requestUrl = (author, count) => `https://openlibrary.org/search.json?q=author:${author} language:eng&fields=key,title,author_name,editions,cover_i&limit=${count}`
-      const authors = ["colleen hoover", "j k rowling", "john green", "stephen king", "leigh bardugo", "khaled hosseini", "tolkien"];
+      const requestUrl = (author, count) =>
+        `https://openlibrary.org/search.json?q=author:${author} language:eng&fields=key,title,author_name,editions,cover_i&limit=${count}`;
+      const authors = [
+        "colleen hoover",
+        "j k rowling",
+        "john green",
+        "stephen king",
+        "leigh bardugo",
+        "khaled hosseini",
+        "tolkien",
+        "agatha christie",
+        "taylor jenkins reid",
+        "christina li",
+        "tashie bhuiyan",
+      ];
       const data = [];
       for (const author of authors) {
-        const response = await fetch(requestUrl(author, 5));
+        const response = await fetch(
+          requestUrl(author, Math.floor(Math.random() * 3) + 2)
+        ); // Random amount of books between 3 and 5
         const json = await response.json();
         if (json.numFound > 0) {
           json.docs.forEach((work) => {
             const id = work.editions.docs[0].key.split("/").slice(-1);
-            const image = `https://covers.openlibrary.org/b/id/${work.editions.docs[0].cover_i}-L.jpg`
+            const image = `https://covers.openlibrary.org/b/id/${work.editions.docs[0].cover_i}-L.jpg`;
             const title = work.editions.docs[0].title;
             const description = work.author_name[0];
             const price = 70;
-            data.push({id, title, image, description, price});
-          })
+            data.push({ id, title, image, description, price });
+          });
         }
       }
-      setData(data);
+      const shuffledData = data.sort(() => Math.random() - 0.5);
+      setData(shuffledData);
       setLoading(false);
-    }
+    };
     fetchData();
-  }, [])
+  }, []);
   if (loading) return "Loading...";
   if (error) return "Error!";
-  return CardWrapper({updateCart: updateCart, productData: data});
+  return CardWrapper({ updateCart: updateCart, productData: data });
 };
 
 export default Fetcher;
