85a36c3f0929ca15adfaebf226ce03ffd4f696ff
Arch <wbhaoui@gmail.com>
2024-04-22 21:19:48 +0100

    Fix error where carousel was not sliding

--- src/components/TextCarousel/TextCarousel.jsx
+++ src/components/TextCarousel/TextCarousel.jsx
@@ -2,17 +2,22 @@ import { useEffect, useRef, useState } from "react";
 import styles from "./TextCarousel.module.css";
 
 const TextCarousel = ({ paragraphs }) => {
+  const firstNode = useRef(null);
   const paragraphNodes = paragraphs.map((paragraph, index) => (
-    <p dangerouslySetInnerHTML={{__html: paragraph}} className={styles.paragraph}></p>
+    <p {...(index === 0 ? {ref: firstNode} : {})} key={index} dangerouslySetInnerHTML={{__html: paragraph}} className={styles.paragraph}></p>
   ));
-  const firstNode = useRef(paragraphNodes[0]);
+  const [paragraphWidth, setParagraphWidth] = useState(0);
   const carouselCount = paragraphs.length;
   const [activeId, setActiveId] = useState(0);
-  const paragraphWidth =firstNode.current.offsetWidth;
   const carouselOffset = -paragraphWidth * activeId;
   const transformStyle = {
     transform: `translateX(${carouselOffset}px)`,
   };
+  useEffect(() => {
+    if (firstNode.current) {
+      setParagraphWidth(firstNode.current.offsetWidth);
+    }
+  }, [firstNode.current.offsetWidth]);
   useEffect(() => {
     const interval = setTimeout(() => {
       if (activeId !== carouselCount - 1) {
@@ -20,7 +25,7 @@ const TextCarousel = ({ paragraphs }) => {
       } else {
         setActiveId(0);
       }
-    }, 40000);
+    }, 4000);
     return () => clearTimeout(interval);
   }, [activeId]);
   return (
